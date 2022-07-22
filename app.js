65fff77064506b9d6da64f99f9041c5a78b7973c
Petri Laine <petri.laine@almamedia.fi>
2022-07-22 13:56:37 +0300

    Add version and healthcheck routes, use heathcheck in Heroku deploy

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const app = express()
 
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
+const version = process.env.npm_package_version
 
 app.use(express.static('dist'))
 
+app.get('/health', (_req, res) => {
+  res.send('ok')
+})
+app.get('/version', (_req, res) => {
+  res.send(`version: ${version}`) // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('server started on port 5000')
+  console.log(`Server started on port ${PORT}\n🚀 v${version}`)
 })
