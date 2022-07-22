61c7b7f75464b943d45607b9a86cbaec7fd0478e
Petri Laine <petri.laine@almamedia.fi>
2022-07-22 14:39:20 +0300

    Add PR condition to pipeline.yml

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 const express = require('express')
 const app = express()
-
+const { version } = require('./package.json')
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
-const version = process.env.npm_package_version
 
 app.use(express.static('dist'))
 
 app.get('/health', (_req, res) => {
   res.send('ok')
 })
+
 app.get('/version', (_req, res) => {
-  res.send(`version: ${version}`) // change this string to ensure a new version deployed
+  res.send(`version: ${version.replaceAll('"', '')}`) // change this string to ensure a new version deployed
 })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log(`Server started on port ${PORT}\n🚀 v${version}`)
+  console.log(
+    `Server started on port ${PORT}\n🚀 v${version.replaceAll('"', '')}`
+  )
 })
